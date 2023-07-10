c04eb99e54c44c5e89d10891b6eda77f2eecb6a4
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-07-10 21:26:00 +0200

    fetch todos implemented

---
+++ src/reducers/todos.ts
@@ -0,0 +1,16 @@
+import { Todo, FetchTodosAction, DeleteTodoAction } from '../actions';
+import { ActionTypes } from '../actions/types';
+
+export const todosReducer = (
+  state: Todo[] = [],
+  action: FetchTodosAction | DeleteTodoAction
+) => {
+  switch (action.type) {
+    case ActionTypes.fetchTodos:
+      return action.payload;
+    case ActionTypes.deleteTodo:
+      return state.filter((todo) => todo.id !== action.payload);
+    default:
+      return state;
+  }
+};
